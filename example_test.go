1af3055c02cf1e93a7e0230aea9184d8d31f58f1
evgeniy <1124grom@gmail.com>
2021-09-06 16:51:00 +0300

    Added examples

---
+++ example_test.go
@@ -0,0 +1,18 @@
+package currency_test
+
+import (
+	"fmt"
+	currency "github.com/gromey/iso-currency"
+)
+
+func ExampleByAlphabeticCode() {
+	ccy, err := currency.ByAlphabeticCode("JPY")
+	fmt.Println(ccy, err)
+	// Output: &{JPY 392 0 Yen [JAPAN]} <nil>
+}
+
+func ExampleByNumericCode() {
+	ccy, err := currency.ByNumericCode("392")
+	fmt.Println(ccy, err)
+	// Output: &{JPY 392 0 Yen [JAPAN]} <nil>
+}
